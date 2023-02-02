02cd77851cdce3b63f2afd69e9faa03d9643b481
Reese-Yoon <hyun5no@naver.com>
2023-02-03 00:37:47 +0900

    springboot init

---
+++ luxury_goods/src/main/java/com/project/luxury_goods/LuxuryGoodsApplication.java
@@ -0,0 +1,13 @@
+package com.project.luxury_goods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuxuryGoodsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuxuryGoodsApplication.class, args);
+	}
+
+}
---
+++ luxury_goods/src/test/java/com/project/luxury_goods/LuxuryGoodsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.luxury_goods;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuxuryGoodsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
