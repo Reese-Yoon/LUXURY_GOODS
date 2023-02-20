b258a8e82a4c631245099e8ba34b304f38a449d6
sknyuki <sknyuki009@gmail.com>
2023-02-20 18:17:58 +0900

    홈배너 aweswiper

--- goodsfront/src/views/HomeView.vue
+++ goodsfront/src/views/HomeView.vue
@@ -1,15 +1,23 @@
 <template>
-  <header-view></header-view>
+  <div>
+    <v-container>
+      <header-view></header-view>
+      <br />
+      <home-banner></home-banner>
+    </v-container>
+  </div>
 </template>
 
 <script>
 import HeaderView from "@/components/Home/HeaderView.vue";
+import HomeBanner from "@/components/Home/HomeBanner.vue";
 
 export default {
   name: "HomeVeiw",
 
   components: {
     HeaderView,
+    HomeBanner,
   },
 };
 </script>
